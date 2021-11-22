6cd390c3358d464bc6b5e68dbe2b5b95ecd6f4ba
Jack <jclarkeparker@gmail.com>
2021-11-23 12:42:46 +1300

    First attempt at deleting todos from list

--- todo.rb
+++ todo.rb
@@ -123,3 +123,14 @@ post "/lists/:list_id/todos" do
     redirect "/lists/#{@list_id}"
   end
 end
+
+# Delete a todo from a list
+post "/lists/:list_id/todos/:todo_id/destroy" do
+  list_id = params[:list_id].to_i
+  list = session[:lists][list_id]
+  
+  todo_id = params[:todo_id].to_i
+  list[:todos].delete_at(todo_id)
+
+  redirect "/lists/#{list_id}"
+end
